6905a969082c02fc1508e2c49c106bad618edb00
Holly C <hollyc@Hollys-MBP.home>
2021-06-26 19:40:37 -0400

    updated to reflect figma

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './homeStyle.css'
 
 // Replace with local path, ensurre that "./" is at the beginning if sourced from same directory
 import logo from './images/tigertradelogo.png'
@@ -8,21 +9,25 @@ import {
 
 } from 'reactstrap';
 
-// This will be where to make the "Home" page
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                <img src={logo} className="logo" alt="Tiger Trade"/>
-            
-                <h2>Trade goods with other Princeton students!</h2> 
+            <div id="main">
+                
+                <img src={logo} alt="Tiger Trade logo"/>
+                <br></br><br></br><br></br>
+                <span><b>Trade goods with other Princeton students!</b></span>  
+                <br></br><br></br><br></br>
 
+                <button class="button" id="loginButton"><span><b>LOG IN!</b></span></button>
 
-                <h2>Questions or concerns? 
-                    <button id="feedback"><b>Send us feedback!</b></button>
-                </h2> 
+                <div id="feedback">
+                    <span>Questions or concerns?</span>
+                    <button id="feedbackButton"><b><u>Send us feedback!</u></b></button>
+                </div>
+                <div id="bottomBar"> </div>
             </div>
-        )
+        ) 
     }
 }
 
