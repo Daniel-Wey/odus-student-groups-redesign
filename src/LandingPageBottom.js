b57a7e302476b13828647cfee3c7a842ef2b49ff
zoenver <zoha1234@gmail.com>
2021-07-26 14:13:08 -0400

    landing page bottom

--- src/LandingPageBottom.js
+++ src/LandingPageBottom.js
@@ -12,18 +12,33 @@ import {
 class LandingPageBottom extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div >
+                <div >
                 <span id = "listings"> All listings</span>
                 </div>
 
                 <div id= "itemList"> 
-                    <div id="item1"></div>
-                    <span id="name1"> Item Name</span>
-                    <span id="amt1"> $$</span>
-                    <div id="item2"></div>
-                    <div id="item3"></div>
-                    <div id="item4"></div>
+                    <div id="item1">
+                    <span class="name"> Item Name</span>
+                    <span class="amt"> $$</span>
+
+                    </div>
+                   
+                    <div id="item2">
+                        
+                    <span class="name"> Item Name</span>
+                    <span class="amt"> $$</span>
+                    </div>
+                   
+                    <div id="item3">
+                    <span class="name"> Item Name</span>
+                    <span class="amt"> $$</span>
+                    </div>
+
+                    <div id="item4">
+                    <span class="name"> Item Name</span>
+                    <span class="amt"> $$</span>
+                    </div>
                     <div id="item5"></div>
                     <div id="item6"></div>
                     <div id="item7"></div>
