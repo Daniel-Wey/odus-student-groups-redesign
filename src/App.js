c748caa1a5bfe58d890d96b2e3559255a3f0260c
djapp18 <dennisjacob@icloud.com>
2021-08-31 18:54:09 -0400

    Satus

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './Styles/App.css';
 import StartPage from './StartPage.js'
 import Nav from './HoagieNav.tsx'
 
-
+// TODO: Implement next.js
+// TODO: Organize components
+// TODO: Download stylesheets from mail.hoagie.io, set up ssh for Git
 
 
 function App() {
